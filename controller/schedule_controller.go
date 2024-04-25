582102fb94e4c14db0d929d8ee0802298d412a55
Akevin12 <kevinthony74@gmail.com>
2024-04-25 11:42:25 +0700

    update

--- controller/schedule_controller.go
+++ controller/schedule_controller.go
@@ -4,21 +4,6 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// GetSchedule godoc
-// @Summary Get Schedule
-// @Description Get a schedule by movie_id and schedule_id.
-// @Tags Customer
-// @Param movieId path string true "movie id"
-// @Accept json
-// @Produce json
-// @Success 200 {object} models.MovieSchedulesResponse
-// @Failure 400 {object} models.ErrorResponse "Bad Request"
-// @Failure 401 {object} models.ErrorResponse "Unauthorized"
-// @Failure 500 {object} models.ErrorResponse "Internal Server Error"
-// @Router /movies/{movieId}/schedules [get]
-func GetSchedules(c *gin.Context) {
-}
-
 // GetSchedulesByMovieId godoc
 // @Summary Get all schedules for a movie by ID
 // @Description Get all schedules for a movie by ID
